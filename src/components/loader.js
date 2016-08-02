1503393118c43b0f121cedc5f09c71bb76e36d49
Lan Qingyong <lanqy1314@gmail.com>
2016-08-02 22:43:18 +0800

    Update loader.js

--- src/components/loader.js
+++ src/components/loader.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Spinner from 'react-spinner';
 class Loader extends React.Component {
   render() {
-    var styles = {width:'60px',height:'60px',position:'absolute',top:'50%',marginLeft:'-30px',marginTop:'-30px',left:'50%',background:'black'}
+    var styles = {width:'60px',height:'60px',position:'absolute',borderRadius:'5px',top:'50%',marginLeft:'-30px',marginTop:'-30px',left:'50%',background:'black'}
     return (
       <div style={styles}>
         <Spinner />
